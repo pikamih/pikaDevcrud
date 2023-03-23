9ad29df6589abd5fb33580a80f7adea5d52df230
Joseph Ipanaque <jipanaque@canvia.com>
2023-03-22 23:21:31 -0500

    Proyecto SpringBoot - Productos

---
+++ crud-productos/src/test/java/com/pikaDevcrud/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikaDevcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
